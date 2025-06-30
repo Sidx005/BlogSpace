80947ac2fd8da17e92f56721660f98fae00016d4
Sidx005 <desaisiddharth1246@gmail.com>
2025-06-30 10:20:09 +0530

    Adding Backend Link

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -15,7 +15,7 @@ import axios from 'axios';
 
 // axiosLocal.js
 export const axiosLocal = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api',
   withCredentials: true,
 });
 
