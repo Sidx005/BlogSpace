238edbe87c4d8c4efb0248096e3ea48ed8870f79
Siddharth Desai <143771414+Sidx005@users.noreply.github.com>
2026-04-28 11:17:45 +0530

    Change gradient direction in Hero component

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   className="absolute  right-2 w-0 md:w-1/4 -bottom-5 object-cover"
   alt=""
 />
-      <motion.h1 initial={{opacity:0,y:50}} transition={{delay:.5,duration:1}} animate={{opacity:1,y:0}} className='text-4xl font-bold dark:bg-gradient-to-r from-white to-white/30 p-1 dark:bg-clip-text dark:text-transparent text-center'>Where Curiosity Meets Cosmos</motion.h1>
+      <motion.h1 initial={{opacity:0,y:50}} transition={{delay:.5,duration:1}} animate={{opacity:1,y:0}} className='text-4xl font-bold dark:bg-gradient-to-t from-white to-white/30 p-1 dark:bg-clip-text dark:text-transparent text-center'>Where Curiosity Meets Cosmos</motion.h1>
       <motion.p initial={{opacity:0}} transition={{delay:.9,duration:2}} animate={{opacity:1}} className='text-lg text-center mt-2'>Unravel the mysteries of the universe with us.</motion.p>
       <Button style={{padding:'20px'}} variant='classic'>Explore the universe </Button>
     </Flex>
